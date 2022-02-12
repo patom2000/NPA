a3ec99730c63a8a4c3be71b9f60ffe851541a1ea
PWic2510 <patompong909@gmail.com>
2022-02-12 17:40:04 +0700

    delete test all
    Co-authored-by: Supakit Khawmeewong <LoukHin@users.noreply.github.com>

--- lab5/test_netmiko.py
+++ lab5/test_netmiko.py
@@ -1,11 +1,5 @@
 from netmikolab import *
 
-def test_all():
-    test_ip()
-    test_subnet()
-    test_status()
-    test_desc()
-
 def test_ip():
     assert get_ip(device, "G0/0") == "172.31.101.4"
     assert get_ip(device, "G0/3") == "unassigned"
@@ -20,7 +14,7 @@ def test_desc():
     assert get_desc(device, "G0/0") == "Connect to G0/2 of S0"
     assert get_desc(device, "G0/1") == "Connect to G0/2 of S1"
     assert get_desc(device, "G0/2") == "Connect to G0/1 of R2"
-    assert get_desc(device, "G0/3") == None
+    assert get_desc(device, "G0/3") == "Not Use"
     print("decription pass")
 
 def test_status():
