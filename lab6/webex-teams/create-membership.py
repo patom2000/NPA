312f20dcfe6c0f530cc9a22c53e4a5b60c391b52
PWic2510 <patompong909@gmail.com>
2022-02-19 15:44:32 +0000

    files for coding

---
+++ lab6/webex-teams/create-membership.py
@@ -0,0 +1,21 @@
+# Fill in this file with the code to create a room membership from the Webex Teams exercise
+import json
+import requests
+
+access_token = "ZjM3OTJhNGItNDkyMS00NWE0LWIzYmMtZDkwMDFkMmVjODkyMWJjZTk1ZWQtMmI5_P0A1_4a252141-f787-4173-a4c9-bde69c553a24"
+room_id = "Y2lzY29zcGFyazovL3VybjpURUFNOnVzLXdlc3QtMl9yL1JPT00vNTg4MjliOTAtOTE5Mi0xMWVjLWI3ZjMtODFlZDdmNGJiODhm"
+url = "https://webexapis.com/v1/memberships"
+person_email = "62070184@it.kmitl.ac.th"
+
+headers = {
+    "Authorization": f'Bearer {access_token}',
+    "Content-Type": "application/json"
+}
+
+params = {
+    'roomId': room_id,
+    'personEmail': person_email
+}
+res = requests.post(url, headers=headers, params=params)
+
+print(json.dumps(res.json(), indent=4))
