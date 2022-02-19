2a85656ab2dda4b46d4658d811de836b20cef675
PWic2510 <patompong909@gmail.com>
2022-02-19 14:15:20 +0000

    edit code for correctness

--- lab6/webex-teams/list-people.py
+++ lab6/webex-teams/list-people.py
@@ -1,14 +1,14 @@
 # Fill in this file with the authentication code from the Webex Teams exercise
 from email import header
+import http
 from os import access
 import requests
 import json
 
 access_token = "ZjM3OTJhNGItNDkyMS00NWE0LWIzYmMtZDkwMDFkMmVjODkyMWJjZTk1ZWQtMmI5_P0A1_4a252141-f787-4173-a4c9-bde69c553a24"
-url = "https://webexapis.com/v1/people/me"
+url = "https://webexapis.com/v1/people/"
 person_id = "Y2lzY29zcGFyazovL3VzL1BFT1BMRS9jNWU1NGY5ZC01YjA2LTQ5OTgtOWI0OC0wODQ5YzM5Zjc3OGU"
-url2 = f'https://webexapis.com/v1/people/{person_id}'
-
+url2 = f"https://webexapis.com/v1/people/{person_id}"
 headers = {
     "Authorization": f'Bearer {access_token}',
     "Content-Type": "application/json"
@@ -17,5 +17,5 @@ params = {
     "email": "62070111@it.kmitl.ac.th"
 }
 
-res = requests.get(url, headers=headers)
+res = requests.get(url2, headers=headers)
 print(json.dumps(res.json(), indent=4))
