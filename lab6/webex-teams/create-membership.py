50eb53647c284bddc4856be2261daf2f477649e5
ปฐมพงษ์ วิมลเจริญ <patompong909@gmail.com>
2022-02-20 00:32:10 +0700

    delete unused code

--- lab6/webex-teams/create-membership.py
+++ lab6/webex-teams/create-membership.py
@@ -6,13 +6,6 @@ access_token = "ZjM3OTJhNGItNDkyMS00NWE0LWIzYmMtZDkwMDFkMmVjODkyMWJjZTk1ZWQtMmI5
 room_id = "Y2lzY29zcGFyazovL3VybjpURUFNOnVzLXdlc3QtMl9yL1JPT00vNTg4MjliOTAtOTE5Mi0xMWVjLWI3ZjMtODFlZDdmNGJiODhm"
 url = "https://webexapis.com/v1/memberships"
 person_email = "62070184@it.kmitl.ac.th"
-"""
-
-access_token = 'your_token_here'
-room_id = 'your_room_id'
-person_email = 'new-user@example.com'
-url = 'https://webexapis.com/v1/memberships'
-"""
 headers = {
     'Authorization': 'Bearer {}'.format(access_token),
     'Content-Type': 'application/json'
