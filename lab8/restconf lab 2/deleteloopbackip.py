5ca968b356cf1ea78f5c8d485ffa45018a170352
PWic2510 <patompong909@gmail.com>
2022-03-17 22:08:24 +0700

    restconf demo

---
+++ lab8/restconf lab 2/deleteloopbackip.py
@@ -0,0 +1,18 @@
+import json
+import requests
+requests.packages.urllib3.disable_warnings()
+
+api_url = "https://10.0.15.111/restconf/data/ietf-interfaces:interfaces/interface=Loopback2/ietf-ip:ipv4/address=10.2.1.1"
+
+headers = { "Accept": "application/yang-data+json", 
+            "Content-type":"application/yang-data+json"
+           }
+basicauth = ("admin", "cisco")
+
+
+resp = requests.delete(api_url, auth=basicauth, headers=headers, verify=False)
+
+if(resp.status_code >= 200 and resp.status_code <= 299):
+    print("STATUS OK: {}".format(resp.status_code))
+else:
+    print('Error. Status Code: {} \nError message: {}'.format(resp.status_code,resp.json()))
